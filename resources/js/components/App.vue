418547efe28716c044275dc1db824a82849e8f79
Joel Bispo <joelbispo04@hotmail.com>
2025-05-19 14:58:29 -0300

    iniciando a parte de resgatar os dados de um formulario

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <form class="row g-3">
+  
     <div
       style="
         display: flex;
@@ -13,26 +13,27 @@
       <!-- Coluna 1 -->
       <div style="display: flex; flex-direction: column; gap: 2rem;">
         <!-- Cadastro do Cliente -->
+        <form class="row g-3" @submit.prevent="enviarFormulario('cliente')">
         <div class="card" style="width: 20rem;">
           <div class="card-body">
             <h5 class="card-title">Cadastro do Cliente</h5>
 
             <label for="nome" class="form-label">Nome</label>
-            <input type="text" class="form-control" id="nome" />
+            <input type="text" class="form-control" id="nome" v-model="formCostumer.name"/>
 
             <div class="row">
               <div class="col-md-6">
                 <label for="cpf" class="form-label">CPF/CNPJ</label>
-                <input type="text" class="form-control" id="cpf" />
+                <input type="text" class="form-control" id="cpf" v-model="formCostumer.cpf"/>
               </div>
               <div class="col-md-6">
                 <label for="telefone" class="form-label">Telefone</label>
-                <input type="text" class="form-control" id="telefone" />
+                <input type="text" class="form-control" id="telefone" v-model="formCostumer.telefone" />
               </div>
             </div>
 
             <label for="email" class="form-label mt-2">Email</label>
-            <input type="email" class="form-control" id="email" />
+            <input type="email" class="form-control" id="email" v-model="formCostumer.email"/>
 
             <div class="mt-3 d-flex justify-content-between">
               <button type="submit" style="width: 140px" class="btn btn-primary">Salvar</button>
@@ -40,23 +41,25 @@
             </div>
           </div>
         </div>
+        </form>
 
         <!-- Cadastro de Produtos -->
+        <form @submit.prevent="enviarFormulario('produto')">
         <div class="card" style="width: 20rem;">
           <div class="card-body">
             <h5 class="card-title">Cadastro de Produtos</h5>
 
             <label for="nome-produto" class="form-label">Nome do Produto</label>
-            <input type="text" class="form-control" id="nome-produto" />
+            <input type="text" class="form-control" id="nome-produto" v-model="formProducts.name" />
 
             <label for="marca" class="form-label mt-2">Marca</label>
-            <select class="form-select" id="marca"></select>
+            <select class="form-select" id="marca" v-model="formProducts.mark"></select>
 
             <label for="descricao" class="form-label mt-2">Descrição</label>
-            <input type="text" class="form-control" id="descricao" />
+            <input type="text" class="form-control" id="descricao" v-model="formProducts.description"/>
 
             <label for="valor" class="form-label mt-2">Valor</label>
-            <input type="text" class="form-control" id="valor" />
+            <input type="text" class="form-control" id="valor" v-model="formProducts.value"/>
 
             <div class="mt-3 d-flex justify-content-between">
               <button type="submit" class="btn btn-primary" style="width: 140px" >Salvar</button>
@@ -64,17 +67,19 @@
             </div>
           </div>
         </div>
+        </form>
       </div>
 
       <!-- Coluna 2 -->
       <div style="display: flex; flex-direction: column; gap: 2rem;">
         <!-- Cadastro de Marcas -->
+        <form @submit.prevent="enviarFormulario('marca')">
         <div class="card" style="width: 20rem;">
           <div class="card-body">
             <h5 class="card-title">Cadastro de Marcas</h5>
 
             <label for="marca-nome" class="form-label">Nome da Marca</label>
-            <input type="text" class="form-control" id="marca-nome" />
+            <input type="text" class="form-control" id="marca-nome" v-model="formBrands.name"/>
 
             <div class="mt-3 d-flex justify-content-between">
               <button type="submit" class="btn btn-primary" style="width: 140px" >Salvar</button>
@@ -82,27 +87,29 @@
             </div>
           </div>
         </div>
+        </form>
 
         <!-- Vendas -->
+        <form @submit.prevent="enviarFormulario('venda')">
         <div class="card" style="width: 20rem;">
           <div class="card-body">
             <h5 class="card-title">Venda</h5>
 
             <label for="venda-cliente" class="form-label">Cliente</label>
-            <select class="form-select" id="venda-cliente"></select>
+            <select class="form-select" id="venda-cliente" v-model="formSale.costumer"></select>
             
             <div class="row">
               <div class="col-md-4">
                 <label for="venda-produto" class="form-label mt-2">Produto</label>
-                <select class="form-select" id="venda-produto"></select>
+                <select class="form-select" id="venda-produto" v-model="formSale.product"></select>
               </div>
               <div class="col-md-4">
                 <label for="quantidade" class="form-label mt-2">Quantidade</label>
-                <input type="text" class="form-control" id="quantidade" />
+                <input type="text" class="form-control" id="quantidade" v-model="formSale.qty" />
               </div>
               <div class="col-md-4">
                 <label for="valor-total" class="form-label mt-2">Valor Total</label>
-                <input type="text" class="form-control" id="valor-total" />
+                <input type="text" class="form-control" id="valor-total" v-model="formSale.total"/>
               </div>
             </div>
             
@@ -113,8 +120,51 @@
             </div>
           </div>
         </div>
+        </form>
       </div>
     </div>
-  </form>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      formCostumer: {
+        name: "",
+        cpf: "",
+        telefone: "",
+        email: ""
+      },
+      formProducts: {
+        name: "",
+        mark: "",
+        description: "",
+        value: "",
+      },
+      formBrands: {
+        name: ""
+      },
+      formSale: {
+        customer: "",
+        product: "",
+        qty: "",
+        total: ""
+      }
+    }
+  },
+  methods: {
+    enviarFormulario(tipo) {
+      if(tipo === 'cliente'){
+        console.log('Dados dos Clientes', this.formCostumer)
+      } else if(tipo === 'produto'){
+        console.log('Dados dos Produtos', this.formProducts)
+      } else if(tipo === "marca"){
+        console.log("Marca", this.formBrands)
+      } else if(tipo === "venda"){
+        console.log("Dados da venda",  this.formSale)
+      }
+    }
+  }
+}
+</script>
 
